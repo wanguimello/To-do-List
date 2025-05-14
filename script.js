ac154d4b5c6f65785cbb71e4f20b6e18caf7c215
Joyce Kimani <joycewangui590@gmail.com>
2025-05-14 19:03:43 +0300

    adds comments

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+//Adds a new ask to the list when the butto  is clicked
 function addTask(){
-    if(inputBox.value === ''){
+    //Checks if input is empty
+    if(inputBox.value === ''){ 
         alert("You must write something!");
     }
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        //span acts as a delete buttton
         let span = document.createElement("span");
+        //\u00d7 is the Unicode character for "×".
         span.innerHTML = "\u00d7";
         li.appendChild(span); 
     }
     inputBox.value = "";
+    //Save data in local storage
     saveData();
 }
 
+//Allows interaction with the list
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
